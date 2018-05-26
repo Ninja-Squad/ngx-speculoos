5bbcc42c7caa1bfd95db9b3a331f76730ceeec61
cexbrayat <cedric@ninja-squad.com>
2018-05-26 15:09:53 +0200

    refactor: improve custom Jasmine typings

--- projects/ngx-speculoos/src/custom-jasmine-types.d.ts
+++ projects/ngx-speculoos/src/custom-jasmine-types.d.ts
@@ -1,6 +1,8 @@
 declare namespace jasmine {
   // the official CustomMatcher interface doesn't have negativeCompare
+  // can be removed once PR https://github.com/DefinitelyTyped/DefinitelyTyped/pull/26051 is released
   interface CustomMatcher {
-    negativeCompare?: (actual: any, ...expected: any[]) => CustomMatcherResult;
+    negativeCompare?<T>(actual: T, expected: T, ...args: any[]): CustomMatcherResult;
+    negativeCompare?(actual: any, ...expected: any[]): CustomMatcherResult;
   }
 }
