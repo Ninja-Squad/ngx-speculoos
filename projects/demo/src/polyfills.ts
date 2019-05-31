5873e257ed533c3e5cbca53b3391ef0e09147623
jnizet <jb@ninja-squad.com>
2019-05-31 09:38:53 +0200

    chore: upgrade angular and angular cli to latest 7.x version

--- projects/demo/src/polyfills.ts
+++ projects/demo/src/polyfills.ts
@@ -13,7 +13,6 @@
  *
  * Learn more in https://angular.io/guide/browser-support
  */
-
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
@@ -56,8 +55,6 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
