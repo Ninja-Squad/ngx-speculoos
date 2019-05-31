0e139d9c8e5b79812c1bad595a9a8424b43b0194
jnizet <jb@ninja-squad.com>
2019-05-31 12:01:05 +0200

    docs: release-2.0.0

--- documentation/js/menu-wc.js
+++ documentation/js/menu-wc.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 customElements.define('compodoc-menu', class extends HTMLElement {
     constructor() {
         super();
@@ -11,106 +12,101 @@ customElements.define('compodoc-menu', class extends HTMLElement {
     }
 
     render(isNormalMode) {
-        let tp = lithtml.html(`<nav>
-    <ul class="list">
-        <li class="title">
-            <a href="index.html" data-type="index-link">ngx-speculoos documentation</a>
-        </li>
-        <li class="divider"></li>
-        ${ isNormalMode ? `<div id="book-search-input" role="search">
-    <input type="text" placeholder="Type to search">
-</div>
-` : '' }
-        <li class="chapter">
-            <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
-            <ul class="links">
-                    <li class="link">
-                        <a href="overview.html" data-type="chapter-link">
-                            <span class="icon ion-ios-keypad"></span>Overview
-                        </a>
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">ngx-speculoos documentation</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                        <li class="link">
+                            <a href="overview.html" data-type="chapter-link">
+                                <span class="icon ion-ios-keypad"></span>Overview
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="index.html" data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>README
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="changelog.html"  data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>CHANGELOG
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="license.html"  data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>LICENSE
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="dependencies.html" data-type="chapter-link">
+                                <span class="icon ion-ios-list"></span>Dependencies
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#classes-links"' :
+                            'data-target="#xs-classes-links"' }>
+                            <span class="icon ion-ios-paper"></span>
+                            <span>Classes</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse" ${ isNormalMode ? 'id="classes-links"' : 'id="xs-classes-links"' }>
+                            <li class="link">
+                                <a href="classes/ComponentTester.html" data-type="entity-link">ComponentTester</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/TestButton.html" data-type="entity-link">TestButton</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/TestElement.html" data-type="entity-link">TestElement</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/TestHtmlElement.html" data-type="entity-link">TestHtmlElement</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/TestInput.html" data-type="entity-link">TestInput</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/TestSelect.html" data-type="entity-link">TestSelect</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/TestTextArea.html" data-type="entity-link">TestTextArea</a>
+                            </li>
+                        </ul>
                     </li>
-                    <li class="link">
-                        <a href="index.html" data-type="chapter-link">
-                            <span class="icon ion-ios-paper"></span>README
-                        </a>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#miscellaneous-links"'
+                            : 'data-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse" ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/functions.html" data-type="entity-link">Functions</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
                     </li>
-                    <li class="link">
-                            <a href="changelog.html"
-                        data-type="chapter-link">
-                            <span class="icon ion-ios-paper"></span>CHANGELOG
-                        </a>
-                    </li>
-                    <li class="link">
-                            <a href="license.html"
-                        data-type="chapter-link">
-                            <span class="icon ion-ios-paper"></span>LICENSE
-                        </a>
-                    </li>
-                    <li class="link">
-                        <a href="dependencies.html"
-                            data-type="chapter-link">
-                            <span class="icon ion-ios-list"></span>Dependencies
+                    <li class="divider"></li>
+                    <li class="copyright">
+                        Documentation generated using <a href="https://compodoc.app/" target="_blank">
+                            <img data-src="images/compodoc-vectorise.png" class="img-responsive" data-type="compodoc-logo">
                         </a>
                     </li>
             </ul>
-        </li>
-        <li class="chapter">
-            <div class="simple menu-toggler" data-toggle="collapse"
-            ${ isNormalMode ? 'data-target="#classes-links"' : 'data-target="#xs-classes-links"' }>
-                <span class="icon ion-ios-paper"></span>
-                <span>Classes</span>
-                <span class="icon ion-ios-arrow-down"></span>
-            </div>
-            <ul class="links collapse"
-            ${ isNormalMode ? 'id="classes-links"' : 'id="xs-classes-links"' }>
-                    <li class="link">
-                        <a href="classes/ComponentTester.html" data-type="entity-link">ComponentTester</a>
-                    </li>
-                    <li class="link">
-                        <a href="classes/TestButton.html" data-type="entity-link">TestButton</a>
-                    </li>
-                    <li class="link">
-                        <a href="classes/TestElement.html" data-type="entity-link">TestElement</a>
-                    </li>
-                    <li class="link">
-                        <a href="classes/TestHtmlElement.html" data-type="entity-link">TestHtmlElement</a>
-                    </li>
-                    <li class="link">
-                        <a href="classes/TestInput.html" data-type="entity-link">TestInput</a>
-                    </li>
-                    <li class="link">
-                        <a href="classes/TestSelect.html" data-type="entity-link">TestSelect</a>
-                    </li>
-                    <li class="link">
-                        <a href="classes/TestTextArea.html" data-type="entity-link">TestTextArea</a>
-                    </li>
-            </ul>
-        </li>
-        <li class="chapter">
-            <div class="simple menu-toggler" data-toggle="collapse"
-            ${ isNormalMode ? 'data-target="#miscellaneous-links"' : 'data-target="#xs-miscellaneous-links"' }>
-                <span class="icon ion-ios-cube"></span>
-                <span>Miscellaneous</span>
-                <span class="icon ion-ios-arrow-down"></span>
-            </div>
-            <ul class="links collapse"
-            ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
-                    <li class="link">
-                      <a href="miscellaneous/functions.html" data-type="entity-link">Functions</a>
-                    </li>
-                    <li class="link">
-                      <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
-                    </li>
-            </ul>
-        </li>
-        <li class="divider"></li>
-        <li class="copyright">
-                Documentation generated using <a href="https://compodoc.app/" target="_blank">
-                            <img data-src="images/compodoc-vectorise.svg" class="img-responsive" data-type="compodoc-logo">
-                </a>
-        </li>
-    </ul>
-</nav>`);
+        </nav>
+        `);
         this.innerHTML = tp.strings;
     }
 });
